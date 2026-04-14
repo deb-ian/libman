2feb941631c5e5e6724f8feb532ea6e24ffcc73d
Fa1c0n <78836383+nav-github01001@users.noreply.github.com>
2026-04-14 11:25:50 +0530

    add new updated schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,4 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "auth" (
-	"user_id"	INTEGER NOT NULL UNIQUE,
-	"username"	TEXT NOT NULL,
-	"email"	TEXT NOT NULL,
-	"salt"	TEXT NOT NULL,
-	"password_hash"	TEXT NOT NULL,
-	PRIMARY KEY("user_id")
-);
 CREATE TABLE IF NOT EXISTS "book_type" (
 	"isbn"	INTEGER NOT NULL,
 	"title"	TEXT NOT NULL,
@@ -20,10 +12,20 @@ CREATE TABLE IF NOT EXISTS "books" (
 	"book_id"	INTEGER NOT NULL UNIQUE,
 	"isbn"	INTEGER NOT NULL,
 	"condition"	TEXT NOT NULL,
-	"borrower_id"	INTEGER,
 	PRIMARY KEY("book_id"),
-	FOREIGN KEY("borrower_id") REFERENCES "members"("member_id"),
 	FOREIGN KEY("isbn") REFERENCES "book_type"("isbn")
+);
+CREATE TABLE IF NOT EXISTS "borrowhistory" (
+	"borrow_id"	INTEGER NOT NULL UNIQUE,
+	"book_id"	INTEGER NOT NULL,
+	"member_id"	INTEGER NOT NULL,
+	"issued_at"	TEXT NOT NULL,
+	"issuer_id"	INTEGER NOT NULL,
+	"due_by"	TEXT NOT NULL,
+	PRIMARY KEY("borrow_id"),
+	FOREIGN KEY("book_id") REFERENCES "books"("book_id"),
+	FOREIGN KEY("issuer_id") REFERENCES "superuser"("issuer_id"),
+	FOREIGN KEY("member_id") REFERENCES "members"("member_id")
 );
 CREATE TABLE IF NOT EXISTS "members" (
 	"member_id"	INTEGER NOT NULL UNIQUE,
@@ -32,5 +34,14 @@ CREATE TABLE IF NOT EXISTS "members" (
 	"phone_number"	TEXT NOT NULL,
 	"member_since"	INTEGER NOT NULL,
 	PRIMARY KEY("member_id")
+);
+CREATE TABLE IF NOT EXISTS "superuser" (
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL,
+	"salt"	TEXT NOT NULL,
+	"password_hash"	TEXT NOT NULL,
+	"role"	TEXT NOT NULL DEFAULT 'HELPER',
+	PRIMARY KEY("user_id")
 );
 COMMIT;
